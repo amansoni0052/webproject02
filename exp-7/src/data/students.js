bfb982cc23648ad5988d5d5f09aa1037118f4d1b
amansoni0052 <notamansoni@gmail.com>
2026-06-23 22:08:56 +0530

    Add files via upload

---
+++ exp-7/src/data/students.js
@@ -0,0 +1,22 @@
+const students = [
+  {
+    id: 1,
+    name: "Amitabh Bachhan",
+    course: "Computer Science",
+    marks: 98,
+  },
+  {
+    id: 2,
+    name: "Shahrukh khan",
+    course: "Information Technology",
+    marks: 92,
+  },
+  {
+    id: 3,
+    name: "Sri Devi",
+    course: "Electronics",
+    marks: 97,
+  },
+];
+
+export default students;
